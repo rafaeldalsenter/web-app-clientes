ba8ab99b14f38bca934f35c4670beeeb41fc501d
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2020-04-03 15:07:31 -0300

    Adicionado arquivos do repositório anterior :)

---
+++ WebAppClientes.Infra.CrossCutting.Ioc/IocBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppClientes.Infra.Data;
+using WebAppClientes.Repositories;
+using WebAppClientes.Services;
+
+namespace WebAppClientes.Infra.CrossCutting.Ioc
+{
+    public class IocBootstrapper
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IClienteServices, ClienteServices>();
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+        }
+    }
+}
---
+++ WebAppClientes.Repositories/IClienteRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAppClientes.Domain;
+
+namespace WebAppClientes.Repositories
+{
+    public interface IClienteRepository
+    {
+        Cliente GetById(Guid id);
+
+        IEnumerable<Cliente> Get();
+
+        void Add(Cliente cliente);
+
+        void Update(Cliente cliente);
+
+        void SaveChanges();
+    }
+}
